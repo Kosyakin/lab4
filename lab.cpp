7510175fd399244fb0f87c6210e12ff257cea12e
Kosyakin <62693680+Kosyakin@users.noreply.github.com>
2020-04-08 10:13:08 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -1,11 +1,10 @@
 #include "pch.h"
 #include <iostream>
-#include <iostream>
 #include <fstream>
 using namespace std;
 // Создание матрицы
 
-double **Create(double n, double m) {
+double **Create(int n, int m) {
 	double **M = new double *[n];
 	for (int i = 0; i < n; ++i) {
 		M[i] = new double[m];
@@ -15,7 +14,7 @@ double **Create(double n, double m) {
 
 // Удаление матрицы
 
-void Free(double **M, double n) {
+void Free(double **M, int n) {
 	for (int i = 0; i < n; ++i) {
 		delete[] M[i];
 	}
@@ -24,7 +23,7 @@ void Free(double **M, double n) {
 
 //ввод матрицы
 
-void Input(double **M, double n, double m) {
+void Input(double **M, int n, int m) {
 
 	for (int i = 0; i < n; i++) {
 
@@ -47,7 +46,7 @@ void Input(double **M, double n, double m) {
 
 //вывод матрицы (я его отключил)
 
-void Print(double ** M, double n, double m, double *f) {
+void Print(double ** M, int n, int m, double *f) {
 
 	for (int i = 0; i < n; ++i) {
 
@@ -128,7 +127,7 @@ double *gauss(double **M, double *f, int n)
 
 // Метод Якоби
 
-void Jacobi(double n, double** M)
+void Jacobi(int n, double** M)
 {
 	const double eps = 0.0001;
 	double* TempX = new double[n];
@@ -146,7 +145,7 @@ void Jacobi(double n, double** M)
 
 	do {
 		for (int i = 0; i < n; i++) {
-			TempX[i] = F[i];
+			TempX[i] = F[i]/10;
 			for (int g = 0; g < n; g++) {
 				if (i != g)
 					TempX[i] -= M[i][g] * X[g];
@@ -164,24 +163,27 @@ void Jacobi(double n, double** M)
 
 	cout << endl << "Metod Jacobi" << endl;
 	ofstream out("ans2.dat");
+	
 	for (int i = 0;i < 100;i++) {
-		cout << TempX[i] / 10 << endl;
-		out	 << TempX[i] / 10 << endl;
+		cout << TempX[i] << endl;
+		out	 << TempX[i] << endl;
+	
 	}
+	cout << "норма векторов невязки";
+		for (int i = 0;i < 100;i++) {
+			cout << i + 1 - TempX[i] * 10 << endl;
+		}
 	out.close();
 	delete[] TempX;
 }
-
-
-
 
 int main()
 {
 	double *x;
 
 	//слишком много придется менять если изменить на инт, у меня несколько функций завязано на этих значениях
-	double n;
-	double m;
+	int n;
+	int m;
 
 	n = 100;
 	m = 100;
@@ -199,11 +201,15 @@ int main()
 
 	cout << endl << "Metod Gaussa" << endl;
 	ofstream out("ans1.dat");
-	for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 
-		cout << x[i] << endl;
-		out << x[i] << endl;
-	}
+			cout << x[i] << endl;
+			out << x[i] << endl;
+		}
+		cout << "норма векторов невязки";
+		for (int i = 0;i < 100;i++) {
+			cout << i + 1 - x[i] * 10 << endl;
+		}
 	out.close();
 
 	Jacobi(n, A);
@@ -212,206 +218,6 @@ int main()
 
 
 	Free(A, n);
-
-
-	system("pause");
-	return 0;
-}
-
-using namespace std;
-// Создание матрицы
-
-double **Create(double n, double m) {
-	double **M = new double *[n];
-	for (int i = 0; i < n; ++i) {
-		M[i] = new double[m];
-	}
-	return M;
-}
-
-// Удаление матрицы
-
-void Free(double **M, double n) {
-	for (int i = 0; i < n; ++i) {
-		delete[] M[i];
-	}
-	delete[] M;
-}
-
-//ввод матрицы
-
-void Input(double **M, double n, double m) {
-
-		for (int i = 0; i < n; i++){
-		
-			for (int j = 0; j < m; j++) {
-				double c =1.0+j;
-				
-				if (i == j) {
-					M[i][j] = 10;
-				}
-				else if(j>i+1) {
-					M[i][j] = 0;
-				}
-				else {
-					M[i][j]=1/c;
-				}
-
-			}
-	}
-}
-
-//вывод матрицы
-void Print(double ** M, double n, double m, double *f ) {
-
-	for (int i = 0; i < n; ++i) {
-
-		for (int j = 0; j < m; ++j) {
-			if (M[i][j] != 0) {
-				cout << M[i][j] << "*x" << j + 1 << "  +  ";
-			}
-		}
-		f[i] = i + 1;
-		cout << "=" << f[i] << endl;
-	}
-}
-
-
-//Метод гауса
-
-double *gauss(double **M, double *f, int n)
-{
-	double *x, max;
-	int k, index;
-	const double eps = 0.0001;  // точность
-	x = new double[n];
-	k = 0;
-	while (k < n)
-	{
-		// Поиск строки с максимальным a[i][k]
-		max = abs(M[k][k]);
-		index = k;
-		for (int i = k + 1; i < n; i++)
-		{
-			if (abs(M[i][k]) > max)
-			{
-				max = abs(M[i][k]);
-				index = i;
-			}
-		}
-		// Перестановка строк
-		if (max < eps)
-		{
-			// нет ненулевых диагональных элементов
-			return 0;
-		}
-		for (int j = 0; j < n; j++)
-		{
-			double temp = M[k][j];
-			M[k][j] = M[index][j];
-			M[index][j] = temp;
-		}
-		double temp = f[k];
-		f[k] = f[index];
-		f[index] = temp;
-		// Нормализация уравнений
-		for (int i = k; i < n; i++)
-		{
-			double temp = M[i][k];
-			if (abs(temp) < eps) continue; // для нулевого коэффициента пропустить
-			for (int j = 0; j < n; j++)
-				M[i][j] = M[i][j] / temp;
-			f[i] = f[i] / temp;
-			if (i == k)  continue; // уравнение не вычитать само из себя
-			for (int j = 0; j < n; j++)
-				M[i][j] = M[i][j] - M[k][j];
-			f[i] = f[i] - f[k];
-		}
-		k++;
-	}
-	// обратная подстановка
-	for (k = n - 1; k >= 0; k--)
-	{
-		x[k] = f[k];
-		for (int i = 0; i < k; i++)
-			f[i] = f[i] - M[i][k] * x[k];
-	}
-
-	return x;
-}
-
-
-// Метод Якоби
-
-void Jacobi(double n, double** M)
-{
-	const double eps = 0.0001;
-	double* TempX = new double[n];
-	double norm; //норма, определяемая как наибольшая разность компонент столбца иксов соседних итераций.
-
-
-	double* F = new double[n];
-	double* X = new double[n];
-	
-	for (int i = 0;i < n;i++) {
-		F[i] = i + 1;  //Значение функции
-		X[i] = 1;  //Начальное приближение
-	}
-
-
-	do {
-		for (int i = 0; i < n; i++) {
-			TempX[i] = F[i];
-			for (int g = 0; g < n; g++) {
-				if (i != g)
-					TempX[i] -= M[i][g] * X[g];
-			}
-			TempX[i] /= M[i][i];
-		}
-		norm = fabs(X[0] - TempX[0]);
-		for (int h = 0; h < n; h++) {
-			if (fabs(X[h] - TempX[h]) > norm)
-				norm = fabs(X[h] - TempX[h]);
-			X[h] = TempX[h];
-		}
-	} while (norm > eps);
-	cout << endl << "Metod Jacobi" << endl;
-	for (int i = 0;i < n;i++) {
-		cout <<"x"<<i+1<<"="<<TempX[i]/10 << endl;
-	}
-	delete[] TempX;
-}
-
-
-
-
-int main()
-{
-	double n, m, *x;
-
-	cout << "lines: ";
-	cin >> n;
-	cout << "rows:  ";
-	cin >> m;
-	
-	double **A = Create(n, m);
-	double *f;
-	f = new double[n];
-	
-	Input( A, n, m );
-	Print(A, n, m, f);
-
-
-	x=gauss(A, f, n);
-	cout << endl << "Metod Gaussa" << endl;
-	for (int i = 0; i < n; i++)
-		cout << "x" << i+1 << "=" << x[i] << endl;
-	cin.get(); 
-
-	Jacobi(n,A);
-
-
-	Free(A, n);	
 
 
 	system("pause");
